3a2e9145fc18e69b4f8a5bc2b6047f453a0dd366
rebecacojocarescu <161582865+rebecacojocarescu@users.noreply.github.com>
2025-11-11 16:46:38 +0200

    oculus settings + working controllers + video in readme

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ WEEK 4 : https://docs.google.com/document/d/1FNug8vaRyZqlnHwkdZRYCURtmaYNZccGJPc
 
 WEEK 5:https://www.canva.com/design/DAG3pTp56H8/IeMtUD7rkcpthbTMrgR3AQ/edit?utm_content=DAG3pTp56H8&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton
 Video: https://www.youtube.com/watch?v=xn82Clmib7I
+
+WEEK 7: https://youtu.be/Wzh7j5-NIpw
---
+++ RaftingProject/Assets/Scripts/SimpleBoat.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class SimpleBoat : MonoBehaviour
+{
+    public float speed = 5f;
+    public float turnSpeed = 40f;
+
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        transform.Translate(Vector3.forward * move);
+
+        float turn = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime;
+        transform.Rotate(0f, turn, 0f);
+    }
+}
